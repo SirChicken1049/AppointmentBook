d0c7485b740941a7416592422a09df5e02bab82e
EvanBurman <evanb77@nycstudents.net>
2025-09-26 11:50:02 -0400

    AppointmentBook

--- src/AppointmentBook.java
+++ src/AppointmentBook.java
@@ -7,5 +7,14 @@ public class AppointmentBook {
         return schedule[period -1][minute];
         // A private method? Interesting.
     }
-    public int findFreeBlock(int period, int duration)
+    public int findFreeBlock(int period, int duration) {
+        return 0;
+    }
+    public boolean makeAppointment(int startPeriod, int endPeriod, int duration){
+        return false;
+    }
+    public void printPeriod(int period) {
+        for(int i = 0; i < schedule[period - 1].length; i++)
+            System.out.println(i + " " + schedule[period - 1][1]);
+    }
 }

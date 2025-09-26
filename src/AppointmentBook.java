fc905a91fe2a753b97416ed5a77af1eabf085e3e
EvanBurman <evanb77@nycstudents.net>
2025-09-26 11:32:41 -0400

    AppointmentBook

---
+++ src/AppointmentBook.java
@@ -0,0 +1,11 @@
+public class AppointmentBook {
+    private boolean[][] schedule;
+    public AppointmentBook(boolean[][] schedule){
+        this.schedule = schedule;
+    }
+    private boolean isMinuteFree(int period, int minute){
+        return schedule[period -1][minute];
+        // A private method? Interesting.
+    }
+    public int findFreeBlock(int period, int duration)
+}

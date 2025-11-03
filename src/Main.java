3b0e2383830275c3ac23db877fc3699c0e7c6797
EvanBurman <evanb77@nycstudents.net>
2025-11-03 11:41:00 -0500

    TEST CASES

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ public class Main {
         for(int i = 50; i < 60; i++) schedule[1][i] = true;
         a.printPeriod(2);
         System.out.println(a.findFreeBlock(2,15));
+        System.out.println(a.findFreeBlock(2,9));
+        System.out.println(a.findFreeBlock(2,20));
         schedule = new boolean[8][60];
         for(int i = 25; i <30; i++) schedule[1][i] = true;
         for(int i = 0; i <15; i++) schedule[2][i] = true;
@@ -28,6 +30,8 @@ public class Main {
         for(int i = 44; i <60; i++) schedule[3][i] = true;
         b.printPeriod(4);
         System.out.println(b.makeAppointment(2,4,22));
+        System.out.println(b.makeAppointment(3,4,3));
+        System.out.println(b.makeAppointment(2,4,30));
         b.printPeriod(4);
     }
     }
